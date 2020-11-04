c00d0ca1b6502d4f9a30da3b8a6ac454a2ab0506
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2020-11-04 18:34:24 +0200

    Update ToDoService.java

--- src/main/java/com/softserve/itacademy/service/ToDoService.java
+++ src/main/java/com/softserve/itacademy/service/ToDoService.java
@@ -1,14 +1,16 @@
 package com.softserve.itacademy.service;
 
+import com.softserve.itacademy.exception.EntityNotFoundException;
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.ToDo;
 
 import java.util.List;
 
 public interface ToDoService {
-    ToDo create(ToDo todo);
-    ToDo readById(long id);
-    ToDo update(ToDo todo);
-    void delete(long id);
+    ToDo create(ToDo todo) throws NullEntityReferenceException;
+    ToDo readById(long id) throws EntityNotFoundException;
+    ToDo update(ToDo todo) throws EntityNotFoundException, NullEntityReferenceException;
+    void delete(long id) throws EntityNotFoundException;
 
     List<ToDo> getAll();
     List<ToDo> getByUserId(long userId);
