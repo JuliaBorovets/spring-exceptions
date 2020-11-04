10fd7c1038fda7a087557d46e4ff5538f4dae551
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2020-11-04 18:33:58 +0200

    Update UserService.java

--- src/main/java/com/softserve/itacademy/service/UserService.java
+++ src/main/java/com/softserve/itacademy/service/UserService.java
@@ -1,14 +1,16 @@
 package com.softserve.itacademy.service;
 
+import com.softserve.itacademy.exception.EntityNotFoundException;
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.User;
 
 import java.util.List;
 
 public interface UserService {
-    User create(User user);
-    User readById(long id);
-    User update(User user);
-    void delete(long id);
+    User create(User user) throws NullEntityReferenceException;
+    User readById(long id) throws EntityNotFoundException;
+    User update(User user) throws EntityNotFoundException, NullEntityReferenceException;
+    void delete(long id) throws EntityNotFoundException;
     List<User> getAll();
 
 }
